8780663577a13854961ae83d137bbd2ceb4504a9
Nadja Zollo <nz@ninjaconcept.com>
2019-11-20 17:34:15 +0100

    Go with p5.js and draw simple 2D color grid. Add basic interaction.

--- 2019/19/colors.js
+++ 2019/19/colors.js
@@ -1,77 +1,70 @@
 /*
-  This sketch draws a simple 2D grid with differently colored rects.
+  This p5.js sketch draws a simple 2D grid with differently colored rects.
   Interaction is provided to allow for changing the density of the 
   grid cells and for coloring to grid cells randomly:
-  - Key 'w': Increase grid density.
-  - Key 's': Decrease grid density.
+  - Key 'UP_ARROW': Increase grid density.
+  - Key 'DOWN_ARROW': Decrease grid density.
   - Key 'c': Toggle between fixed and random grid colors.
 */
 
-const gridLength = 12.5
-const intensity = 1
-const pulseColor = false
+let gridLength = 12.5
+let intensity = 1
+let pulseColor = false
 
-const x = 400 / gridLength
-const y = 400 / gridLength
 
-// void setup() {
-//   size(400, 400);
-//   background(209);
-// }
-
-d3.select('svg')
-  .selectAll('rect')
-  .data(d3.range(x * y))
-  .enter()
-  .append('rect')
-  .attr('x', d => (d % x) * gridLength)
-  .attr('y', d => Math.floor(d / x) * gridLength)
-  .attr('width', gridLength)
-  .attr('height', gridLength)
-  .attr('stroke', '#fff')
-  .attr('fill', d =>
-    d3.hsl(intensity * d * gridLength, intensity * (y - d * gridLength), 100)
-  )
-
-/*
-void draw() {
-  frameRate(2);
-  // Define color with hue, saturation, brightness.
-  colorMode(HSB, width, width / 2, 100);
-  // Create the grid.
-  for (int i = 0; i < width / gridLength; i++) {
-    for (int j = 0; j < height / gridLength; j++) {  
-      fill(intensity * i * gridLength, intensity * (height - j * gridLength), 100);
-      rect(i * gridLength, j * gridLength, gridLength, gridLength);
-      if (pulseColor) {
-        intensity = random(0, 1);
-      }
-    }
-  } 
+function setup() {
+  const width = windowWidth > 400 ? 400 : windowWidth
+  const height = width
+  createCanvas(width, height)
+  frameRate(2)
 }
 
-void keyPressed() {
-  // Increase (here: multiply by 2) the density of grid cells by 
-  // pressing the 'w' key, but only if the current grid length 
-  // ranges from 12.5 up to but not including 200.
-  if (key == 'w') {
-    if (gridLength >= 12.5 && gridLength < 200) {
+function draw() {
+  background(220)
+  colorMode(HSB, width, width / 2, 100)
+  // Create the grid.
+  for (let i = 0; i < width / gridLength; i++) {
+    for (let j = 0; j < height / gridLength; j++) {
+      fill(
+        intensity * i * gridLength,
+        intensity * (height - j * gridLength),
+        100
+      )
+      rect(i * gridLength, j * gridLength, gridLength, gridLength)
+      // Set a white stroke.
+      // In HSB this is brightness 0% and saturation 100%; hue can be anything
+      stroke(220, 0, 100)
+      if (pulseColor) {
+        intensity = random(0, 1)
+      }
+    }
+  }
+}
+
+function keyPressed() {
+  console.log(keyCode)
+  // Decrease the density of grid cells (i.e. multiply by 2 and increase 
+  // grid length) by pressing the 'DOWN_ARROW' key, but only if the current
+  // grid length ranges from 12.5 up to but not including 400.
+  if (keyCode === DOWN_ARROW) {
+    if (gridLength >= 12.5 && gridLength < 400) {
       gridLength = gridLength * 2;
     }
   }
-  
-  // Decrease (here: divide by 2) the density of grid cells by
-  // pressing the 's' key, but only if the current grid length
-  // ranges from 200 down to but not including 12.5.
-  if (key == 's') {
-    if (gridLength <= 200 && gridLength > 12.5) {
+  // Increase the density of grid cells (i.e. divide by 2 and decrease grid
+  // length) by pressing the 'UP_ARROW' key, but only if the current grid length
+  // ranges from 400 down to but not including 12.5.
+  if (keyCode === UP_ARROW) {
+    if (gridLength <= 400 && gridLength > 12.5) {
       gridLength = gridLength / 2;
     } 
   }
   
   // Toggle color pulsing by pressing the 'c' key.
-  if (key == 'c') {
+  if (keyCode === 67) {
     pulseColor = pulseColor ? false : true;
   }
+
 }
-*/
+
+
